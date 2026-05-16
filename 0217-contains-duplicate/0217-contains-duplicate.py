bdd4919155237c51f973c4a9197efe4a857cfad1
Ch.Siri <chepurisiri5@gmail.com>
2026-05-16 15:21:40 +0530

    0217-contains-duplicate .py

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -3,7 +3,7 @@ class Solution:
         if len(nums)==1:
             return False
         nums.sort()
-        for i in range(0,len(nums)):
+        for i in range(1,len(nums)):
             if nums[i]==nums[i-1]:
                 return True
         return False
