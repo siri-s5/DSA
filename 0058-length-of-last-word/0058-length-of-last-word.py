f4272d500536501cacb6110e939be8f41e2973d9
Ch.Siri <chepurisiri5@gmail.com>
2026-05-17 21:55:36 +0530

    0058-length-of-last-word .py

---
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -0,0 +1,9 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        count=0
+        s=s.strip()
+        for i in range(len(s)-1,-1,-1):
+            if s[i]==" ":
+                break
+            count+=1       
+        return count
