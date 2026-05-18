ed3b4a15a5412b645d6e4859076f3c74212f1be4
Ch.Siri <chepurisiri5@gmail.com>
2026-05-18 22:19:52 +0530

    0049-group-anagrams .py

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,7 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+        for s in strs:
+            key = tuple(sorted(s))  # or ''.join(sorted(s))
+            groups[key].append(s)
+        return list(groups.values())
