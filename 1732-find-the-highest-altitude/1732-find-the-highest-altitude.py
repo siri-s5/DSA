acbefd33998bea967ef3873aa3eaaff4273107cb
Ch.Siri <chepurisiri5@gmail.com>
2026-06-19 21:10:53 +0530

    1732-find-the-highest-altitude .py

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -1,10 +1,11 @@
 class Solution:
-    def largestAltitude(self, gain):
-        n = len(gain)
-        mx = 0
-        for i in range(n + 1):
-            alt = 0
-            for j in range(i):
-                alt += gain[j]
-            mx = max(mx, alt)
-        return mx
+    def largestAltitude(self, gain: List[int]) -> int:
+        res = 0
+        curr = 0
+
+        for change in gain:
+            curr += change
+            res = max(curr, res)
+
+        return res
+        
