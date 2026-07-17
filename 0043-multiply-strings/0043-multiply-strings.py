6cfc340dbf6ab23c3fdea02cdb9b7568f62bc764
Ch.Siri <chepurisiri5@gmail.com>
2026-07-17 22:17:00 +0530

    0043-multiply-strings .py

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,17 @@
+class Solution:
+    def multiply(self,num1: str, num2: str) -> str:
+        if num1=='0' or num2=='0':
+            return '0'
+        def s(num):
+            ans=0
+            for i in num:
+                ans=ans*10+(ord(i)-ord('0'))
+            return ans
+        def t(s):
+            res=''
+            while s>0:
+                a=s%10
+                s=s//10
+                res=chr(ord('0')+a)+res
+            return res
+        return t(s(num1)*s(num2))
