a650195fbb5f0279c0a1b8f6d53c7346213068b6
Ch.Siri <chepurisiri5@gmail.com>
2026-07-20 10:02:20 +0530

    0204-count-primes .py

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -2,13 +2,13 @@ class Solution:
     def countPrimes(self, n: int) -> int:
         if n<=2:
             return 0
-        a=[True]*n
+        a=[True]*(n+1)
         a[0]=a[1]=False
         count=0
         for i in range(2,n):
             if a[i]==True:
                 count+=1
-                for j in range(i*i,n,i):
-                    a[j]=False
+            for j in range(i*i,n,i):
+                a[j]=False
         return count
 #O(nlogn)
