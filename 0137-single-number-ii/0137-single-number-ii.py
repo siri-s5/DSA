683e2c0dcdc36da59e7a125be5fc5861d41002ac
Ch.Siri <chepurisiri5@gmail.com>
2026-07-27 10:13:17 +0530

    0137-single-number-ii .py

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,8 +1,14 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        freq={}
-        for i in nums:
-            freq[i]=freq.get(i,0)+1
-        for num in freq:
-            if freq[num]==1:
-                return num
+        #freq={}
+        #for i in nums:
+        #    freq[i]=freq.get(i,0)+1
+        #for num in freq:
+        #    if freq[num]==1:
+        #        return num
+        nums.sort()
+        i=0
+        n=len(nums)
+        while i<n-1 and nums[i]==nums[i+1]:
+            i+=3
+        return nums[i]
