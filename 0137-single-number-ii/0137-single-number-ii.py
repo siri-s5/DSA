6d5ade981bf8f4bf77906ab675cf8745fed41ee7
Ch.Siri <chepurisiri5@gmail.com>
2026-07-27 11:10:08 +0530

    0137-single-number-ii .py

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -6,9 +6,20 @@ class Solution:
         #for num in freq:
         #    if freq[num]==1:
         #        return num
-        nums.sort()
-        i=0
-        n=len(nums)
-        while i<n-1 and nums[i]==nums[i+1]:
-            i+=3
-        return nums[i]
+        #nums.sort()
+        #i=0
+        #n=len(nums)
+        #while i<n-1 and nums[i]==nums[i+1]:
+        #    i+=3
+        #return nums[i]
+        ans = 0
+        for i in range(32):
+            count = 0
+            for num in nums:
+                if (num >> i) & 1:
+                    count += 1
+            if count % 3!=0:
+                ans |= (1 << i)
+        if ans>=2**31:
+            ans-=2**32
+        return ans
